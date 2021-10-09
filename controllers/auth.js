375d58a75ac0ec234fd5666bb49ba8290f6d0911
Ares Yolao <aresyolao@hotmail.com>
2021-10-08 23:50:18 -0500

    se agrega google sign in

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,8 @@
-const { response } = require("express");
+const { response, request, json } = require("express");
 const bcrypjs = require('bcryptjs')
 const Usuario = require('../models/usuario');
 const { generarJWT } = require("../helpers/gener-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async (req, res = response) => {
 
@@ -30,7 +31,7 @@ const login = async (req, res = response) => {
             })
         }
         //Generar JWT
-const token = await generarJWT(usuario.id);
+        const token = await generarJWT(usuario.id);
 
         res.json({
             usuario,
@@ -45,6 +46,63 @@ const token = await generarJWT(usuario.id);
     }
 }
 
+
+
+const googleSignIn = async (req = request, res = response) => {
+
+    const { id_token } = req.body;
+    // console.log(id_token)
+
+    try {
+    
+        // const googleUser = googleVerify(id_token);
+
+        // console.log(googleUser)
+
+        const {nombre,correo,img} = await googleVerify(id_token);
+        let usuario = await Usuario.findOne({ correo });
+
+        if (!usuario) {
+            //crear usuario
+            const data = {
+                nombre,
+                correo,
+                img,
+                password: ':P',
+                google: true
+            };
+
+
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+
+        //Si el usuario en BD
+
+        if (!usuario.estado) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador , usuario bloqueado'
+            });
+        }
+   
+        const token = await generarJWT(usuario.id);
+
+
+        res.json({
+            usuario,
+            token
+        })
+    } catch (error) {
+console.log(error)
+        res.status(400).json({
+            ok: false,
+            msg: 'El token no se pudo verificar'
+        })
+    }
+
+
+}
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
